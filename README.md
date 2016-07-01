5fb17136b311dfc8687e3569b056e4510bf9de9a
AdamD2 <adam_douglas2@hotmail.com>
2016-07-01 11:47:57 +1000

    Initial commit. This includes the MIT license, a readme, a main program which is set up to be coded with comments detailing what will be added, and a bitmapped image of the car that will be used in the game.

---
+++ README.md
@@ -0,0 +1,8 @@
+This is an adaptation of the Atari 2600 game 'Dragster' by activision.
+
+The goal of the game is to race to the finish line in the lowest possible time 
+by changing up gears with good timing. Don't rev up too high though, or you'll
+blow the engine!
+
+You will need to have the pygame module installed to run the program from the
+source code.
---
+++ dragRace.py
@@ -0,0 +1,31 @@
+
+
+def runGame():
+    # Initialize game, settings and screen object
+
+
+
+    # Make the car
+
+
+
+    # Initialize the timer and gear text
+
+
+
+    # Start the main loop for the game
+    while True:
+        # Check for keypresses
+
+
+
+        # Update the position of the car
+
+
+
+        # Update the screen
+
+
+        pass
+
+runGame()
